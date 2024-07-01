a1d0dbe93600920ab75363c70495a1efdfce6ae8
Soura10 <souradeeproy.10@gmail.com>
2024-07-01 12:56:33 +0530

    model is ready

---
+++ src/llm.py
@@ -0,0 +1,25 @@
+import os
+#import python_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#from google.colab import userdata
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+
+#def load_model():
+def ask_bot(message):
+    
+    llm = ChatGoogleGenerativeAI(model="gemini-pro")
+    response = llm.invoke(message)
+
+    return response.content
+
+if __name__=="__main__":
+    print("Welcome to my chatbot")
+    message = ask_bot("what is the meaning of large language models")
+    print(message)
